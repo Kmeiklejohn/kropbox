36e85d199148d3450ae09a27633e5fc5712ccd16
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2019-04-04 11:06:26 -0400

    file system

--- kropbox/manager/models.py
+++ kropbox/manager/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from mptt.models import MPTTModels, TreeForeignKey
-
-
-class FileObject(MPTTModel):
+from kropbox.profile import KropboxUser
+class Folder(MPTTModel):
 	parent = TreeForeignkey('self', related_name='children', null=True, blank=True)
 	name = models.CharField()
-	file_type = models.CharField(Choices=(('file', 'file'), ('folder', 'folder')))
-	encoding = models.CharField(blank=True, null=True)
-    
+	folder_type = models.CharField()
+	owner = models.ForeignKey(KropboxUser, on_delete=models.CASCADE)
+
+class FileObject(models.Model):
+	folder = TreeForeingKey(Folder)
+	name = models.CharField()
+	encoding = models.CharField()
+	
